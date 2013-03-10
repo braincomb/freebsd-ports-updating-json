166d06de7fdca6167393313542d7c82e70b97ff9
Aleksandr Melentiev <zapper@gmail.com>
2013-03-10 00:18:02 -0800

    Delay instant search by 200ms.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 var App = angular.module('App', ['loading']);
- 
+
 App.controller('DisplayController', function($scope, $http, $timeout) {
-	$http.get('UPDATING.json').then(function(result){
-		$scope.entries = result.data;
-	});
+  $http.get('UPDATING.json').then(function(result){
+    $scope.entries = result.data;
+  });
+
+  // bind filter
+  $scope.filterText = '';
+
+  var tempFilterText = '', filterTextTimeout;
+
+  $scope.$watch('searchText', function (val) {
+    if (filterTextTimeout) $timeout.cancel(filterTextTimeout);
+
+    tempFilterText = val;
+    filterTextTimeout = $timeout(function() {
+      $scope.filterText = tempFilterText;
+    }, 200)
+  });
+
 });
 
 App.filter('newlines', function () {
-    return function(text) {
-        return text.replace(/\n/g, '<br/>');
-    }
+  return function(text) {
+    return text.replace(/\n/g, '<br/>');
+  }
 });
