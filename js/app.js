eb59e9c14a4d26a506ad08cddff83491abb0fea8
Aleksandr Melentiev <zapper@gmail.com>
2013-03-11 01:43:24 -0700

    Format dates so it is easier to read.

--- js/app.js
+++ js/app.js
@@ -35,3 +35,9 @@ App.filter('newlines', function () {
     return text.replace(/\n/g, '<br/>');
   }
 });
+
+App.filter('dates', function () {
+  return function(text) {
+    return text.replace(/^(\d{4})(\d{2})(\d{2})/g, "$1-$2-$3");
+  }
+});
