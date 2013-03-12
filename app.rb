c03a7adec796e45816789ed33f3807dfb66d5892
Aleksandr Melentiev <zapper@gmail.com>
2013-03-12 02:12:07 -0700

    Added option to parse UPDATING via remote URL, now default. (#3)

--- app.rb
+++ app.rb
@@ -1,23 +1,33 @@
 #!/usr/bin/env ruby
-require 'json'
 
 # Author: Aleksandr Melentiev
 # URL: http://github.com/braincomb/freebsd-ports-updating-json
-# Description: parse /usr/ports/UPDATING and generate JSON
+# Description: parses UPDATING file and generates JSON
+# Usage: run manually or setup a crontab at 1 hour interval
+
+require 'json'
+require 'open-uri'
+require 'openssl'
 
 INPUT_FILE = "/usr/ports/UPDATING"
-OUTPUT_FILE = "UPDATING.json"
+INPUT_URL = "https://raw.github.com/freebsd/freebsd-ports/master/UPDATING"
+OUTPUT_FILE = File.dirname(__FILE__) + "/UPDATING.json"
+
+# Uses INPUT_URL by default.
+# Set to false if you want to use your local /usr/ports/UPDATING file.
+USE_REMOTE = true
 
 def parse_updating_file
-  hash = { :updating => [] }
+  hash = { :last_updated => Time.now, :updating => [] }
   date = affects = author = content = ''
-  id = 0
+  index = 0
   getcontent = false
-
-  IO.foreach(INPUT_FILE) do |line|
+  
+  remote_data = open(INPUT_URL, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE) if USE_REMOTE == true
+  IO.foreach(USE_REMOTE ? remote_data : INPUT_FILE) do |line|
     if line =~ /^\d{8}(:)/
       if !content.empty?
-        hash[:updating] << { :id => id += 1, :date => date, :affects => affects, :author => author, :content => content }
+        hash[:updating] << { :index => index += 1, :date => date, :affects => affects, :author => author, :content => content }
       end
     getcontent = false
     content = ''
@@ -40,9 +50,9 @@ def create_json(hash)
   end
 end
 
-if File.file?(INPUT_FILE)
+if USE_REMOTE == false and !File.file?(INPUT_FILE)
+  puts "ERROR: " + INPUT_FILE + " was not found or is not a valid file."
+else
   hash = parse_updating_file
   create_json(hash)
-else
-  puts "ERROR: " + INPUT_FILE + " was not found or is not a valid file."
 end
